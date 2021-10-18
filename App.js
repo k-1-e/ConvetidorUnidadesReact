4e93b13afaaf2e58810196a7bea1caeeac689d47
k-1-e <obregonkevin1351@gmail.com>
2021-10-18 16:57:09 -0600

    configurando la clasificacion de las unidades

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Convertidor } from './src/components/Convertidor';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Convertidor/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor :	'#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/Convertidor.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react"
+import { View, Text, TextInput, StyleSheet, Button,  } from 'react-native';
+
+export const Convertidor = () => {
+    // varaibles de estado
+    const [origen, setOrigen] = useState('cm')
+    const [destino, setDestino] = useState('mt')
+    const [convertir, setConvertir] = useState('0')
+    const [resultado, setResultado] = useState('0')
+
+        
+    
+    const handleChangenConvertir = (texto: string) => {
+        const numero = parseFloat(texto)
+        setConvertir(numero)       
+    }
+    //manejador de evento del boton 
+    const handleCalcular = () => {
+    
+        if (origen === 'cm' && destino === 'mt' ) {
+            const result = convertir / 100
+            setResultado(result)
+        } 
+        else if(origen === 'mm' && destino === 'cm') {
+            const result = convertir / 10.000
+            setResultado(result)
+        } else if (origen === 'mm' && destino === 'mt') {
+            const result = convertir / 1000.0
+            setResultado(result)
+        } else if (origen === 'mm' && destino === 'km') {
+            const result = convertir / 1000000
+            setResultado(result)
+        }
+
+        else if (origen === 'mt' && destino === 'cm') {
+            const result = convertir * 100
+            setResultado(result)
+        }
+
+        
+    }
+    return(
+        // asignacion de las varaibles a caja de texto
+       <View>
+           
+            <Text>Origen {origen}</Text>
+            <TextInput
+            defaultValue={origen}
+            style={styles.input} 
+            maxLength={2}
+            onChangeText={setOrigen}
+            //manejador por defecto setOrigen 
+            />
+
+             <Text>Destino {destino} </Text>
+            <TextInput
+                 defaultValue={destino}
+                 style={styles.input} 
+                 onChangeText={setDestino}
+             />
+              <Text>Convertir</Text>
+            <TextInput
+            defaultValue={convertir.toString()}
+            style={styles.input} 
+            onChangeText={
+                    (text) => handleChangenConvertir(text)} />
+
+            <Text>Resultado</Text>
+            <TextInput 
+            defaultValue={resultado.toString()}
+            style={styles.inputError} />
+
+            
+            <Button
+                title="Calcular"
+                onPress= {handleCalcular}
+                //manejador por defecto del boton calcular
+            />
+            
+       </View>
+    )
+}
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 3,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderColor: 'black',
+    },
+    inputError:{
+        borderWidth: 3,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderColor: 'black',
+    },
+    
+})
